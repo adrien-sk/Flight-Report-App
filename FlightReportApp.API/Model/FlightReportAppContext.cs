5fa35586a47b43dac3ea7685a9cbabcfde97b927
Adrien_Sk <skrzypczak.adrien@gmail.com>
2021-11-07 14:53:35 +0100

    MVP Architecture

---
+++ FlightReportApp.API/Model/FlightReportAppContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightReportApp.API.Model
+{
+	public class FlightReportAppContext : DbContext
+	{
+		public FlightReportAppContext(DbContextOptions<FlightReportAppContext> options)
+		: base(options)
+		{
+		}
+
+		public DbSet<Plane> Planes { get; set; }
+	}
+}
---
+++ FlightReportApp.API/Model/Plane.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightReportApp.API.Model
+{
+	[Table("Plane")]
+	public class Plane
+	{
+		[Column("id")]
+		public int Id { get; set; }
+
+		[Column("code")]
+		public string Code { get; set; }
+
+		[Column("color")]
+		public string Color { get; set; }
+
+		[Column("first_use_date")]
+		public DateTime? FirstUseDate { get; set; }
+	}
+}
